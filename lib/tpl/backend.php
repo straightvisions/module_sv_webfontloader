7e45668117bf25b9994ebc9e410616098662cd15
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2018-10-10 09:23:53 +0200

    PATCH - STYLES - SETTINGS

--- lib/tpl/backend.php
+++ lib/tpl/backend.php
@@ -1,16 +1,22 @@
 <?php
-	if(current_user_can('activate_plugins')){
-		?>
-        <div class="wrap" id="sv_settings">
-            <div id="sv_header">
-                <div id="sv_logo"><img src="<?php echo $this->get_url_lib_core('assets/logo.png'); ?>" /></div>
-            </div>
-            <h1><?php echo get_admin_page_title(); ?></h1>
-			<?php
-				echo $this->s_fonts_upload->get_form_field();
-				echo static::$settings->get_module_settings_form($this);
-			?>
-        </div>
-		<?php
-	}
+if(current_user_can('activate_plugins')){
+	?>
+	<div class="sv_settings">
+		<!--<div class="sv_side_menu">
+			<a href="#" class="sv_brand"><img src="<?php //echo $this->get_url_lib_core('assets/logo.png'); ?>" /></a>
+		</div>-->
+		<div class="sv_content_wrapper">
+			<div class="sv_content_title">
+				<h1><?php echo get_admin_page_title(); ?></h1>
+			</div>
+			<div class="sv_content">
+				<?php
+					echo static::$s_fonts_upload->get_form_field();
+					echo static::$settings->get_module_settings_form($this);
+				?>
+			</div>
+		</div>
+	</div>
+	<?php
+}
 ?>
