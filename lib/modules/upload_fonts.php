866798f49724ce53cbc82919db3f31b0f6192578
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-01-11 16:16:36 +0100

    Update

--- lib/modules/upload_fonts.php
+++ lib/modules/upload_fonts.php
@@ -1,62 +1,74 @@
 <?php
-	namespace sv_100;
-	
-	/**
-	 * @author			Matthias Reuter
-	 * @package			sv_100
-	 * @copyright		2017 Matthias Reuter
-	 * @link			https://straightvisions.com
-	 * @since			1.0
-	 * @license			See license.txt or https://straightvisions.com
-	 */
-	class sv_webfontloader_upload_fonts extends sv_webfontloader{
-		private $filter								= array(
-			'svg'									=> 'image/svg+xml',
-			'woff'									=> 'application/octet-stream',
-			'woff2'									=> 'application/octet-stream',
-			'eot'									=> 'application/vnd.ms-fontobject',
-			'ttf'									=> 'application/x-font-ttf',
-			'otf'									=> 'application/font-sfnt'
-		);
-		
-		public function __construct(){
-			$this->set_section_title('Webfontloader Upload');
-			$this->set_section_desc('Please reload page after new fonts have been uploaded.');
-			$this->set_section_type('settings');
+namespace sv_100;
 
-			add_action('admin_init', array($this, 'admin_init'));
-			add_action('init', array($this, 'init'));
-		}
-		public function admin_init(){
-			add_filter('upload_mimes', array($this, 'upload_mimes'));
-			$this->load_settings();
-		}
-		public function init(){
-			if(!is_admin()){
-				$this->load_settings();
-			}
-		}
-		public function load_settings(){
-			// Uploaded Fonts
-			$this->s['uploaded_fonts']					= static::$settings->create($this)
-				->set_ID('uploaded_fonts')
-				->set_title(__('Uploaded Fonts', $this->get_module_name()))
-				->load_type('multi_upload')
-				->set_callback(array($this,'fonts_list'))
-				->set_filter(array_keys($this->filter));
-		}
-		public function upload_mimes($mime_types = array()){
-			// @todo: make sure setting upload mimes is affecting current form only
-			return array_merge($mime_types,$this->filter);
-		}
-		public function fonts_list($setting): string{
-			$form				= $setting->form();
-			
-			ob_start();
-			require($this->get_path('lib/tpl/backend_upload.php'));
-			$form .= ob_get_contents();
-			ob_end_clean();
-			
-			return $form;
-		}
+/**
+ * @author			Matthias Reuter
+ * @package			sv_100
+ * @copyright		2017 Matthias Reuter
+ * @link			https://straightvisions.com
+ * @since			1.0
+ * @license			See license.txt or https://straightvisions.com
+ */
+
+class sv_webfontloader_upload_fonts extends sv_webfontloader {
+	private $filter								= array(
+		'svg'									=> 'image/svg+xml',
+		'woff'									=> 'application/octet-stream',
+		'woff2'									=> 'application/octet-stream',
+		'eot'									=> 'application/vnd.ms-fontobject',
+		'ttf'									=> 'application/x-font-ttf',
+		'otf'									=> 'application/font-sfnt'
+	);
+
+	public function __construct() {
+
 	}
+
+	public function init() {
+		// Section Info
+		$this->set_section_title( 'Webfontloader Upload' );
+		$this->set_section_desc( __( 'Please reload page after new fonts have been uploaded.', $this->get_module_name() ) );
+		$this->set_section_type( 'settings' );
+
+		// Action Hooks
+		//add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_filter( 'upload_mimes', array( $this, 'upload_mimes' ) );
+
+		// Loads Settings
+		$this->load_settings();
+	}
+
+	public function admin_init() {
+		// Action Hooks
+		add_filter( 'upload_mimes', array( $this, 'upload_mimes' ) );
+
+		// Loads Settings
+		$this->load_settings();
+	}
+
+	public function load_settings() {
+		// Uploaded Fonts
+		$this->s['uploaded_fonts']					= static::$settings->create( $this )
+			->set_ID( 'uploaded_fonts' )
+			->set_title( __( 'Uploaded Fonts', $this->get_module_name() ) )
+			->load_type( 'multi_upload' )
+			->set_callback( array( $this, 'fonts_list' ) )
+			->set_filter( array_keys( $this->filter ) );
+	}
+
+	public function upload_mimes( $mime_types = array() ) {
+		// @todo: make sure setting upload mimes is affecting current form only
+		return array_merge( $mime_types, $this->filter );
+	}
+
+	public function fonts_list( $setting ): string {
+		$form				= $setting->form();
+
+		ob_start();
+		require( $this->get_file_path( 'lib/tpl/backend_upload.php' ) );
+		$form .= ob_get_contents();
+		ob_end_clean();
+
+		return $form;
+	}
+}
--- sv_webfontloader.php
+++ sv_webfontloader.php
@@ -1,231 +1,255 @@
 <?php
-	namespace sv_100;
-	
-	/**
-	 * @author			Matthias Reuter
-	 * @package			sv_100
-	 * @copyright		2017 Matthias Reuter
-	 * @link			https://straightvisions.com
-	 * @since			1.0
-	 * @license			See license.txt or https://straightvisions.com
-	 */
-	class sv_webfontloader extends init{
-		static $scripts_loaded						= false;
-		private $vendors							= '';
-		private $s_fields							= array(
-			'family_name'							=> 'text',
-			'italic'								=> 'checkbox',
-			'weight'								=> 'select',
-			'active'								=> 'checkbox',
+namespace sv_100;
+
+/**
+ * @author			Matthias Reuter
+ * @package			sv_100
+ * @copyright		2017 Matthias Reuter
+ * @link			https://straightvisions.com
+ * @since			1.0
+ * @license			See license.txt or https://straightvisions.com
+ */
+
+class sv_webfontloader extends init {
+	private $vendors							= '';
+	private $s_fields							= array(
+		'family_name'							=> 'text',
+		'italic'								=> 'checkbox',
+		'weight'								=> 'select',
+		'active'								=> 'checkbox',
+	);
+	private $s_titles							= array();
+	private $s_descriptions						= array();
+	private $s_options							= array();
+
+	public function __construct() {
+
+	}
+
+	public function init() {
+		// Module Info
+		$this->set_module_title( 'SV Webfontloader' );
+		$this->set_module_desc( __( 'This module gives the ability to upload & manage webfonts.', $this->get_module_name() ) );
+
+		// Section Info
+		$this->set_section_title( 'Webfontloader' );
+		$this->set_section_desc( 'Configure Fonts previously uploaded.' );
+		$this->set_section_type( 'settings' );
+
+		// Loads Scripts
+		static::$scripts->create( $this )
+		                ->set_source( $this->get_file_url( 'lib/js/frontend.js' ), $this->get_file_path( 'lib/js/frontend.js' ) )
+		                ->set_is_backend()
+		                ->set_type( 'js' );
+
+		// Font Settings
+		$this->s_titles['family_name']			= __( 'Family Name', $this->get_module_name() );
+		$this->s_titles['italic']				= __( 'italic', $this->get_module_name() );
+		$this->s_titles['weight']				= __( 'Font Weight', $this->get_module_name() );
+		$this->s_titles['active']				= __( 'active', $this->get_module_name() );
+
+		$this->s_descriptions['family_name']	= __( 'Font Family Name, e.g. for CSS', $this->get_module_name() );
+		$this->s_descriptions['italic']			= __( 'If this font is italic version, activate this setting.', $this->get_module_name() );
+		$this->s_descriptions['weight']			= __( 'Please select font weight.', $this->get_module_name() );
+		$this->s_descriptions['active']			= __( 'Only active fonts will be loaded.', $this->get_module_name() );
+
+		$this->s_options['weight']				= array(
+			'100'								=> '100',
+			'200'								=> '200',
+			'300'								=> '300',
+			'400'								=> '400',
+			'500'								=> '500',
+			'600'								=> '600',
+			'700'								=> '700',
+			'800'								=> '800',
+			'900'								=> '900',
 		);
-		private $s_titles							= array();
-		private $s_descriptions						= array();
-		private $s_options							= array();
 
-		public function __construct($path,$url){
-			$this->set_section_title('Webfontloader');
-			$this->set_section_desc('Configure Fonts previously uploaded.');
-			$this->set_section_type('settings');
+		// Includes
+		require_once( $this->get_file_path( 'lib/modules/upload_fonts.php' ) );
 
-			$this->path								= $path;
-			$this->url								= $url;
-			$this->name								= get_class($this);
-			
-			$this->s_titles['family_name']			= __('Family Name', $this->get_module_name());
-			$this->s_titles['italic']				= __('italic', $this->get_module_name());
-			$this->s_titles['weight']				= __('Font Weight', $this->get_module_name());
-			$this->s_titles['active']				= __('active', $this->get_module_name());
-			
-			$this->s_descriptions['family_name']	= __('Font Family Name, e.g. for CSS', $this->get_module_name());
-			$this->s_descriptions['italic']			= __('If this font is italic version, activate this setting.', $this->get_module_name());
-			$this->s_descriptions['weight']			= __('Please select font weight.', $this->get_module_name());
-			$this->s_descriptions['active']			= __('Only active fonts will be loaded.', $this->get_module_name());
-			
-			$this->s_options['weight']				= array(
-				'100'								=> '100',
-				'200'								=> '200',
-				'300'								=> '300',
-				'400'								=> '400',
-				'500'								=> '500',
-				'600'								=> '600',
-				'700'								=> '700',
-				'800'								=> '800',
-				'900'								=> '900',
-			);
-			
-			require_once($this->get_file_path('lib/modules/upload_fonts.php'));
-			$this->upload_fonts						= new sv_webfontloader_upload_fonts();
-			$this->upload_fonts->set_root($this->get_root());
-			$this->upload_fonts->set_parent($this);
-			
-			add_action('admin_init', array($this, 'admin_init'));
-			add_action('wp_head', array($this, 'wp_head'));
-			$this->module_enqueue_scripts();
-			
-			if(!is_admin()){
-				add_action('init', array($this, 'load_settings'));
-			}
-		}
-		public function admin_init(){
-			$this->get_root()->add_section($this);
-			$this->get_root()->add_section($this->upload_fonts);
-			$this->load_settings();
-		}
-		private function font_settings(){
-			$fonts									= $this->upload_fonts->get_settings()['uploaded_fonts']->run_type()->get_data();
-			
-			if($fonts){
-				foreach($fonts as $font){
-					// group by filename without ext
-					$url							= wp_get_attachment_url($font->ID);
-					$filename						= basename($url);
-					$fileparts						= explode('.',$filename);
-					if(is_array($fileparts)) {
-						$name = $fileparts[0];
-						$ext = explode('.', $filename)[count($fileparts) - 1];
-					}else{
-						$name = $filename;
-						$ext = 'no_ext';
-					}
+		// Upload Settings
+		$this->upload_fonts						= new sv_webfontloader_upload_fonts();
+		$this->upload_fonts->set_root( $this->get_root( ));
+		$this->upload_fonts->set_parent( $this );
 
-					if(!isset($this->s[$name])){
-						$this->s[$name]		= array();
-					}
+		// Action Hooks
+		//add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_action( 'wp_head', array( $this, 'wp_head' ) );
 
-					$this->s[$name]['url'][$ext]	= $url;
+		$this->get_root()->add_section( $this );
+		$this->get_root()->add_section( $this->upload_fonts );
+
+		// Loads Settings
+		$this->load_settings();
+	}
+
+	public function admin_init() {
+		$this->get_root()->add_section( $this );
+		$this->get_root()->add_section( $this->upload_fonts );
+		$this->load_settings();
+	}
+
+
+	private function font_settings() {
+		$fonts									= $this->upload_fonts->get_settings()['uploaded_fonts']->run_type()->get_data();
+
+		if($fonts){
+			foreach($fonts as $font){
+				// group by filename without ext
+				$url							= wp_get_attachment_url($font->ID);
+				$filename						= basename($url);
+				$fileparts						= explode('.',$filename);
+				if(is_array($fileparts)) {
+					$name = $fileparts[0];
+					$ext = explode('.', $filename)[count($fileparts) - 1];
+				}else{
+					$name = $filename;
+					$ext = 'no_ext';
 				}
-				$this->font_sub_settings();
-			}
-		}
-		private function font_sub_settings(){
-			// create sub settings
-			if(count($this->s) > 0) {
-				foreach($this->s as $name => $data) {
-					foreach($this->s_fields as $field_id => $field_type){
-						$s = static::$settings->create($this)
-							->set_ID('font_' . $name . '_' . $field_id)
-							->set_title($this->s_titles[$field_id])
-							->set_description($this->s_descriptions[$field_id].'<br />'.__('Filetypes available: ', $this->get_module_name()).implode(',', array_keys($this->s[$name]['url'])));
 
-						if(isset($this->s_options[$field_id])){
-							$s->set_options($this->s_options[$field_id]);
-						}
-
-						$s->load_type($field_type);
-						$this->s[$name]['settings'][$field_id] = $s;
-					}
+				if(!isset($this->s[$name])){
+					$this->s[$name]		= array();
 				}
+
+				$this->s[$name]['url'][$ext]	= $url;
 			}
+			$this->font_sub_settings();
 		}
-		public function load_settings(){
-			$this->font_settings();
-		}
-		public function load_custom_fonts(){
-			$formats								= array(
-				'eot'								=> '',
-				'woff'								=> 'woff',
-				'woff2'								=> 'woff2',
-				'ttf'								=> 'truetype',
-				'otf'								=> 'opentype',
-				'svg'								=> 'svg',
-			);
-			$names									= array();
+	}
 
-			if($this->s) {
-				echo '<style data-sv_100_module="'.$this->get_module_name().'">';
-				foreach ($this->s as $name => $data) {
-					if ($data['settings']['active']->run_type()->get_data() == 1) {
-						$names[$family_name]		= $family_name		= $data['settings']['family_name']->run_type()->get_data();
-						$f = array("\n");
-						$f[] = '@font-face {';
-						$f[] = 'font-family: "' . $family_name . '";';
-						$f[] = 'font-display: fallback;';
+	private function font_sub_settings() {
+		// create sub settings
+		if(count($this->s) > 0) {
+			foreach($this->s as $name => $data) {
+				foreach($this->s_fields as $field_id => $field_type){
+					$s = static::$settings->create($this)
+						->set_ID('font_' . $name . '_' . $field_id)
+						->set_title($this->s_titles[$field_id])
+						->set_description($this->s_descriptions[$field_id].'<br />'.__('Filetypes available: ', $this->get_module_name()).implode(',', array_keys($this->s[$name]['url'])));
 
-
-						// src
-						$urls						= $data['url'];
-						asort($urls);
-						foreach ($urls as $ext => $url) {
-							$f[] = 'src:url("' . $url . '")' .
-								((strlen($formats[$ext]) > 0) ? ' format("' . $formats[$ext] . '");' : ';');
-						}
-
-						// weight
-						$f[] = 'font-weight: ' . $data['settings']['weight']->run_type()->get_data() . ';';
-
-						// italic
-						if ($data['settings']['italic']->run_type()->get_data() == 1) {
-							$f[] = 'font-style: italic;';
-						}
-
-						$f[] = '}';
-						echo implode("\n", $f);
+					if(isset($this->s_options[$field_id])){
+						$s->set_options($this->s_options[$field_id]);
 					}
+
+					$s->load_type($field_type);
+					$this->s[$name]['settings'][$field_id] = $s;
 				}
-				echo '</style>';
-
-				$this->vendors .= 'custom: { families: ["' . implode('","', $names) . '"] }';
-			}
-		}
-		public function wp_head(){
-			$this->load_custom_fonts();
-			// we load typekit in head, but async, so there is no pagespeed penality while it got loaded as fast as possible
-			// to prevent flash of unstyled text (FOUT), some CSS is inserted, too.
-
-			/* // we need to upgrade this snippet if we want to allow loading vendor fonts
-			if(get_theme_mod($this->get_module_name().'_typekit', false)){
-				$this->vendors						.= ' typekit: { id : "'.get_theme_mod($this->get_module_name().'_typekit', '').'" }';
-			}
-			if(get_theme_mod($this->get_module_name().'_google', false)){
-				$this->vendors						.= ' google: { families: '.get_theme_mod($this->get_module_name().'_google', '').' } ';
-			}
-
-			$fontawesome							= get_theme_mod($this->get_module_name().'_fontawesome', false);
-			if($fontawesome && strlen($fontawesome) > 0){
-				$fontawesome						= explode(',', $fontawesome);
-				if($fontawesome && is_array($fontawesome) && count($fontawesome) > 0){
-					if(count($fontawesome) === 3){
-						echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>';
-					}else{
-						foreach($fontawesome as $fa_part){
-							echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>';
-							if($fa_part == 'solid'){
-								echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>';
-							}
-							if($fa_part == 'regular'){
-								echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/regular.js" integrity="sha384-t7yHmUlwFrLxHXNLstawVRBMeSLcXTbQ5hsd0ifzwGtN7ZF7RZ8ppM7Ldinuoiif" crossorigin="anonymous"></script>';
-							}
-							if($fa_part == 'brands'){
-								echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/brands.js" integrity="sha384-sCI3dTBIJuqT6AwL++zH7qL8ZdKaHpxU43dDt9SyOzimtQ9eyRhkG3B7KMl6AO19" crossorigin="anonymous"></script>';
-							}
-						}
-					}
-				}
-			}
-			*/
-			if(strlen($this->vendors) > 0){
-				echo '
-					<script data-sv_100_module="'.$this->get_module_name().'">
-						WebFontConfig =
-						{
-						'.$this->vendors.'
-						}
-						;
-					</script>
-				';
-
-				/* // this reduces pagespeed score, so deactivated
-				echo '
-					<style data-sv_100_module="'.$this->get_module_name().'">
-						html:not(.wf-inactive):not(.wf-active) *{
-							opacity:0 !important;
-						}
-						html *{
-							transition: opacity 0.5s linear;
-						}
-					</style>
-				';
-				*/
 			}
 		}
 	}
+
+	public function load_settings() {
+		$this->font_settings();
+	}
+
+	public function load_custom_fonts() {
+		$formats								= array(
+			'eot'								=> '',
+			'woff'								=> 'woff',
+			'woff2'								=> 'woff2',
+			'ttf'								=> 'truetype',
+			'otf'								=> 'opentype',
+			'svg'								=> 'svg',
+		);
+		$names									= array();
+
+		if($this->s) {
+			echo '<style data-sv_100_module="'.$this->get_module_name().'">';
+			foreach ($this->s as $name => $data) {
+				if ($data['settings']['active']->run_type()->get_data() == 1) {
+					$names[$family_name]		= $family_name		= $data['settings']['family_name']->run_type()->get_data();
+					$f = array("\n");
+					$f[] = '@font-face {';
+					$f[] = 'font-family: "' . $family_name . '";';
+					$f[] = 'font-display: fallback;';
+
+
+					// src
+					$urls						= $data['url'];
+					asort($urls);
+					foreach ($urls as $ext => $url) {
+						$f[] = 'src:url("' . $url . '")' .
+							((strlen($formats[$ext]) > 0) ? ' format("' . $formats[$ext] . '");' : ';');
+					}
+
+					// weight
+					$f[] = 'font-weight: ' . $data['settings']['weight']->run_type()->get_data() . ';';
+
+					// italic
+					if ($data['settings']['italic']->run_type()->get_data() == 1) {
+						$f[] = 'font-style: italic;';
+					}
+
+					$f[] = '}';
+					echo implode("\n", $f);
+				}
+			}
+			echo '</style>';
+
+			$this->vendors .= 'custom: { families: ["' . implode('","', $names) . '"] }';
+		}
+	}
+
+	public function wp_head() {
+		$this->load_custom_fonts();
+		// we load typekit in head, but async, so there is no pagespeed penality while it got loaded as fast as possible
+		// to prevent flash of unstyled text (FOUT), some CSS is inserted, too.
+
+		/* // we need to upgrade this snippet if we want to allow loading vendor fonts
+		if(get_theme_mod($this->get_module_name().'_typekit', false)){
+			$this->vendors						.= ' typekit: { id : "'.get_theme_mod($this->get_module_name().'_typekit', '').'" }';
+		}
+		if(get_theme_mod($this->get_module_name().'_google', false)){
+			$this->vendors						.= ' google: { families: '.get_theme_mod($this->get_module_name().'_google', '').' } ';
+		}
+
+		$fontawesome							= get_theme_mod($this->get_module_name().'_fontawesome', false);
+		if($fontawesome && strlen($fontawesome) > 0){
+			$fontawesome						= explode(',', $fontawesome);
+			if($fontawesome && is_array($fontawesome) && count($fontawesome) > 0){
+				if(count($fontawesome) === 3){
+					echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>';
+				}else{
+					foreach($fontawesome as $fa_part){
+						echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>';
+						if($fa_part == 'solid'){
+							echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>';
+						}
+						if($fa_part == 'regular'){
+							echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/regular.js" integrity="sha384-t7yHmUlwFrLxHXNLstawVRBMeSLcXTbQ5hsd0ifzwGtN7ZF7RZ8ppM7Ldinuoiif" crossorigin="anonymous"></script>';
+						}
+						if($fa_part == 'brands'){
+							echo '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/brands.js" integrity="sha384-sCI3dTBIJuqT6AwL++zH7qL8ZdKaHpxU43dDt9SyOzimtQ9eyRhkG3B7KMl6AO19" crossorigin="anonymous"></script>';
+						}
+					}
+				}
+			}
+		}
+		*/
+		if(strlen($this->vendors) > 0){
+			echo '
+				<script data-sv_100_module="'.$this->get_module_name().'">
+					WebFontConfig =
+					{
+					'.$this->vendors.'
+					}
+					;
+				</script>
+			';
+
+			/* // this reduces pagespeed score, so deactivated
+			echo '
+				<style data-sv_100_module="'.$this->get_module_name().'">
+					html:not(.wf-inactive):not(.wf-active) *{
+						opacity:0 !important;
+					}
+					html *{
+						transition: opacity 0.5s linear;
+					}
+				</style>
+			';
+			*/
+		}
+	}
+}
