65eb2df665f9ef4f32b5554087ab1d6732f7151c
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-12-06 14:30:25 +0100

    UPDATE ICON

--- sv_webfontloader.php
+++ sv_webfontloader.php
@@ -8,9 +8,9 @@
 				 ->load_modules()
 				->set_section_title( $this->get_module_title() )
 				->set_section_desc( $this->get_module_desc() )
-				->set_section_type( 'settings' )
 				->set_section_template_path()
 				->set_section_order(600)
+				->set_section_icon('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22 0h-20v6h1.999c0-1.174.397-3 2.001-3h4v16.874c0 1.174-.825 2.126-2 2.126h-1v2h9.999v-2h-.999c-1.174 0-2-.952-2-2.126v-16.874h4c1.649 0 2.02 1.826 2.02 3h1.98v-6z"/></svg>')
 				->get_root()
 				->add_section( $this );
 
